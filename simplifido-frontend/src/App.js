dba623df99e6d69a34b2c27f0551f0a511340567
Matt Gahrns <matthewgahrns@hotmail.com>
2019-12-30 16:02:13 -0800

    Added some navigation and components for home and login

--- simplifido-frontend/src/App.js
+++ simplifido-frontend/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './navbar';
+import Login from './login';
+import Home from './home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+
+
+
+
+
+  render(){
+    return (
+      <Router>
+        <Route path='/' component={Navbar} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ simplifido-frontend/src/navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const link = {
+    width: '100px',
+    padding: '12px',
+    margin: '0 6px 6px',
+    background: 'blue',
+    textDecoration: 'none',
+    color: 'white',
+}
+
+const Navbar = () => {
+  return(
+  <div>
+    <NavLink
+      to="/"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>);
+}
+
+export default Navbar;
