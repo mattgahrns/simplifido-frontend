093f98f538cf8c8d54da496b3da01ca8f2e24c36
Matt Gahrns <matthewgahrns@hotmail.com>
2020-01-01 22:46:30 -0800

    Started working on getting posts on home page

--- simplifido-frontend/src/home.js
+++ simplifido-frontend/src/home.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
 const Home = () => {
-    return (
-      <div>
-        <h1>Simplifido Home</h1>
-      </div>
-    )
+
+  const getPosts = () => {
+    fetch(`http://localhost:3001/posts`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+        })
+        .then(res => res.json())
+        .then(json => {
+          return json.forEach(post => {
+            return (
+              <h1>{post.title}</h1>
+            )
+          });
+        })
+  }
+
+  return (
+    <div>
+      <h1>Simplifido Home</h1>
+      {getPosts()}
+    </div>
+  )
 }
 
 export default Home;
