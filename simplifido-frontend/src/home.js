818a55e86883e963bb62a3dced5b3768d2453d48
Matt Gahrns <matthewgahrns@hotmail.com>
2020-01-02 08:57:04 -0800

    Posts now appear on the home page

--- simplifido-frontend/src/home.js
+++ simplifido-frontend/src/home.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
-const Home = () => {
+class Home extends React.Component{
 
-  const getPosts = () => {
+  constructor(){
+    super();
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount(){
     fetch(`http://localhost:3001/posts`, {
         method: 'GET',
         headers: {
@@ -10,23 +17,39 @@ const Home = () => {
             Accept: 'application/json',
             Authorization: `Bearer ${localStorage.getItem('token')}`
         }
+      })
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          posts: json
         })
-        .then(res => res.json())
-        .then(json => {
-          return json.forEach(post => {
-            return (
-              <h1>{post.title}</h1>
-            )
-          });
-        })
+      });
   }
 
-  return (
-    <div>
-      <h1>Simplifido Home</h1>
-      {getPosts()}
-    </div>
-  )
+  getPosts = () => {
+    return this.state.posts.map(post => {
+      return (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <h3>{post.activity}</h3>
+          <h4>{post.when}</h4>
+          <p>{post.description}</p>
+          <p>By {post.user.username}</p>
+          <p><strong>Contact info: </strong>{post.user.email}</p>
+        </div>
+      )
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Simplifido Home</h1>
+        <hr/>
+        {this.getPosts()}
+      </div>
+    )
+  }
 }
 
 export default Home;
